6291cbeb4b5eb86cd5a1e62c5b4001bee9a85551
Guilherme <contato.guilhermealves@gmail.com>
2021-11-09 15:34:17 -0300

    Add files via upload

---
+++ telaCadastro/src/layouts/layout1.vue
@@ -0,0 +1,57 @@
+<template>
+  <q-layout view="hHh Lpr fFf"> <!-- Be sure to play with the Layout demo on docs -->
+
+    <!-- (Optional) The Header -->
+    <q-header elevated>
+      <q-toolbar>
+
+      </q-toolbar>
+
+      <q-tabs>
+      </q-tabs>
+    </q-header>
+
+    <!-- (Optional) The Footer -->
+    <q-footer>
+      <q-tabs switch-indicator>
+      </q-tabs>
+
+      <q-toolbar>
+      </q-toolbar>
+    </q-footer>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+    >
+      <q-list>
+
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <!-- This is where pages get injected -->
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+export default {
+  // name: 'LayoutName',
+
+  setup () {
+    const leftDrawerOpen = ref(false)
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer () {
+        leftDrawerOpen.value = !leftDrawerOpen.value
+      }
+    }
+  }
+}
+</script>
---
+++ telaCadastro/src/pages/page1.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="window-height window-width row justify-center items-center">
+    <q-card
+      class="my-card text-white"
+      style="
+        background: radial-gradient(circle, #5fc77b 0%, #2a702b 100%);
+        height: 100vh;
+        width: 100vw;
+      "
+    >
+      <q-card-section>
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Nome"
+        />
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Sobrenome"
+        />
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Endereço"
+        />
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Cidade"
+        />
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Telefone"
+        />
+        <q-input
+          standout="bg-teal text-white"
+          v-model="text"
+          label="Email"
+        />
+      </q-card-section>
+
+      <q-card-section class="window-width row justify-center items-center">
+          <q-btn outline style="color: #0e2b16; margin-left: 2px" label="Enviar" />
+          <q-btn outline style="color: #0e2b16; margin-left: 3px" label="Cancelar" />
+      </q-card-section>
+    </q-card>
+  </div>
+</template>
